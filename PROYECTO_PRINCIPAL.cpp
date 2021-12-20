8c0114c85c854e38f22a4b19146ffbf4e082bf18
Santiago245 <santiagohernandez28@aragon.unam.mx>
2021-12-19 20:27:50 -0600

    Es el Proyecto pero con terminacion cpp

---
+++ PROYECTO_PRINCIPAL.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include 
+
+void main ()
+{
+//Proyecto Final de Ingenieria en Programacion
+int Valor;
+printf("Para una division de numeros enteros ingrese 1\n");
+printf("Para una division con numeros decimales ingrese 2\n");
+printf("Para una multiplicacion de numeros enteros ingrse 3\n");
+printf("Para una multiplicacion con numeros desimales ingrese 4\n");
+scanf("%i",&Valo);
+if (valor == 1);
+{
+}
+if (valor == 2);
+{
+}
+if (valor == 3);
+{
+}
+if (valor == 4);
+{
+}
+}
